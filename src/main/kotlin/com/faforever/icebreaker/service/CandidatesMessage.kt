5c2c657d3affdb0e10a451fcf2a6401c2bd076ee
Brutus5000 <brutus5000@gmx.net>
2025-02-28 09:16:19 +0100

    Adjust candidate message to pion representation

--- src/main/kotlin/com/faforever/icebreaker/service/CandidatesMessage.kt
+++ src/main/kotlin/com/faforever/icebreaker/service/CandidatesMessage.kt
@@ -3,7 +3,8 @@ package com.faforever.icebreaker.service
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
-import com.fasterxml.jackson.annotation.JsonValue
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.databind.node.ObjectNode
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "eventType")
 @JsonSubTypes(
@@ -29,31 +30,7 @@ data class CandidatesMessage(
     override val gameId: Long,
     override val senderId: Long,
     override val recipientId: Long?,
-    val candidates: List<CandidateDescriptor>,
-) : EventMessage {
-
-    enum class CandidateType(@JsonValue val jsonValue: String) {
-        PEER_REFLEXIVE_CANDIDATE("prflx"),
-        SERVER_REFLEXIVE_CANDIDATE("srflx"),
-        RELAYED_CANDIDATE("relay"),
-        HOST_CANDIDATE("host"),
-        LOCAL_CANDIDATE("local"),
-        STUN_CANDIDATE("stun"),
-    }
-
-    @JvmRecord
-    data class CandidateDescriptor(
-        val foundation: String,
-        val protocol: String,
-        val priority: Long,
-        val ip: String?,
-        val port: Int,
-        val type: CandidateType,
-        val generation: Int,
-        val id: String,
-        val relAddr: String?,
-        val relPort: Int,
-    ) : Comparable<CandidateDescriptor> {
-        override operator fun compareTo(other: CandidateDescriptor) = (other.priority - priority).toInt()
-    }
-}
+    // Could be specified more, but these fields are just passed through
+    val session: ObjectNode,
+    val candidates: ArrayNode,
+) : EventMessage
