b6ed6c3971d073d0bed0b375e39aa5ca005df605
Brutus5000 <Brutus5000@gmx.net>
2025-06-09 08:35:52 +0200

    Remove @Provider annotation to fix startup

--- src/main/kotlin/com/faforever/icebreaker/util/ErrorLoggingFilter.kt
+++ src/main/kotlin/com/faforever/icebreaker/util/ErrorLoggingFilter.kt
@@ -3,10 +3,8 @@ package com.faforever.icebreaker.util
 import jakarta.ws.rs.client.ClientRequestContext
 import jakarta.ws.rs.client.ClientResponseContext
 import jakarta.ws.rs.client.ClientResponseFilter
-import jakarta.ws.rs.ext.Provider
 import org.slf4j.Logger
 
-@Provider
 class ErrorLoggingFilter(private val logger: Logger) : ClientResponseFilter {
 
     override fun filter(requestContext: ClientRequestContext, responseContext: ClientResponseContext) {
