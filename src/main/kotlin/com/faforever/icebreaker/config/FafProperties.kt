099d88da219a782bf889999464e2b772510c4372
Brutus5000 <brutus5000@gmx.net>
2025-09-05 20:46:13 +0200

    Add forceRelay flag to sessions

--- src/main/kotlin/com/faforever/icebreaker/config/FafProperties.kt
+++ src/main/kotlin/com/faforever/icebreaker/config/FafProperties.kt
@@ -12,6 +12,11 @@ interface FafProperties {
     fun environment(): String
 
     /**
+     * Tell api clients to force their ICE adapters to use a TURN server
+     */
+    fun forceRelay(): Boolean
+
+    /**
      * Define the header, where to pick the real ip address from. For regular reverse proxies such as nginx or Traefik,
      * this is X-Real-Ip. However, in certain scenarios such as Cloudflare proxy different headers might be required.
      */
