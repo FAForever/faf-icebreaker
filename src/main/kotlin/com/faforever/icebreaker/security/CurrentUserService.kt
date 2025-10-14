dbd7fe2478e2967c8612edfc7bc0b7f89149081d
Joshua Hunt <joshua.edward.hunt@gmail.com>
2025-10-14 17:59:47 +0200

    Use JsonWebToken in CurrentUserService
    In tests, I want to be able to use @JwtSecurity for fake authentication,
    but the current code requires the security identity's principal to be
    the more specific class OidcJwtCallerPrincipal.

--- src/main/kotlin/com/faforever/icebreaker/security/CurrentUserService.kt
+++ src/main/kotlin/com/faforever/icebreaker/security/CurrentUserService.kt
@@ -1,17 +1,15 @@
 package com.faforever.icebreaker.security
 
-import io.quarkus.oidc.runtime.OidcJwtCallerPrincipal
 import io.quarkus.security.identity.SecurityIdentity
 import jakarta.enterprise.context.ApplicationScoped
+import org.eclipse.microprofile.jwt.JsonWebToken
 
 @ApplicationScoped
 class CurrentUserService(
     private val securityIdentity: SecurityIdentity,
 ) {
     fun getCurrentUserId(): Long? {
-        val principal = (securityIdentity.principal as? OidcJwtCallerPrincipal)
-        val subject = principal?.claims?.claimsMap?.get("sub") as? String
-
-        return subject?.toLong()
+        val principal = securityIdentity.principal as? JsonWebToken
+        return principal?.subject?.toLongOrNull()
     }
 }
