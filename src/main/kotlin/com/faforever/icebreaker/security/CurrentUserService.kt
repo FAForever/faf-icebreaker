23798845b91612cfb44b1ad3444b8020ee51b491
Brutus5000 <brutus5000@gmx.net>
2025-10-27 21:30:41 +0100

    Fix CurrentUserService scope & streamline Forbidden handling

--- src/main/kotlin/com/faforever/icebreaker/security/CurrentUserService.kt
+++ src/main/kotlin/com/faforever/icebreaker/security/CurrentUserService.kt
@@ -1,21 +1,22 @@
 package com.faforever.icebreaker.security
 
 import com.faforever.icebreaker.config.FafProperties
+import io.quarkus.security.ForbiddenException
 import io.quarkus.security.identity.SecurityIdentity
 import io.vertx.core.http.HttpServerRequest
-import jakarta.enterprise.context.ApplicationScoped
+import jakarta.enterprise.context.RequestScoped
 import jakarta.ws.rs.core.Context
 import org.eclipse.microprofile.jwt.JsonWebToken
 
-@ApplicationScoped
+@RequestScoped
 class CurrentUserService(
     private val securityIdentity: SecurityIdentity,
     private val fafProperties: FafProperties,
     @Context private val httpRequest: HttpServerRequest,
 ) {
-    fun getCurrentUserId(): Long? {
+    fun requireCurrentUserId(): Long {
         val principal = securityIdentity.principal as? JsonWebToken
-        return principal?.subject?.toLongOrNull()
+        return principal?.subject?.toLongOrNull() ?: throw ForbiddenException("No user id provided")
     }
 
     /**
