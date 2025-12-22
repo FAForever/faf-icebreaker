5122a8ca8742cc8e0b655a10b0911e01cb150592
Joshua Hunt <joshua.edward.hunt@gmail.com>
2025-12-22 10:00:09 +0100

    Await unawaited Unis

--- src/test/kotlin/com/faforever/icebreaker/service/hetzner/HetznerFirewallServiceTest.kt
+++ src/test/kotlin/com/faforever/icebreaker/service/hetzner/HetznerFirewallServiceTest.kt
@@ -11,6 +11,7 @@ import com.faforever.icebreaker.service.hetzner.StubHetznerApiClient
 import com.faforever.icebreaker.sync.waitUntil
 import io.quarkus.test.Mock
 import io.quarkus.test.junit.QuarkusTest
+import io.smallrye.mutiny.Uni
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.context.Dependent
 import jakarta.enterprise.inject.Produces
@@ -21,6 +22,7 @@ import org.eclipse.microprofile.rest.client.inject.RestClient
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import java.time.Duration
 import java.util.Optional
 
 @Dependent
@@ -56,18 +58,18 @@ class HetznerFirewallServiceTest {
 
     @Test
     fun `Add whitelist`() {
-        service.whitelistIpForSession("game/200", userId = 123, ipAddress = "1.2.3.4")
-        runBlocking { waitUntil { hetznerApi.getCallCount() == 1 } }
+        awaitAll(service.whitelistIpForSession("game/200", userId = 123, ipAddress = "1.2.3.4"))
 
+        assertThat(hetznerApi.getCallCount()).isEqualTo(1)
         val allowedIps = hetznerApi.getRulesByFirewallId("fwid")!!.allSourceIps()
         assertThat(allowedIps).isEqualTo(setOf("1.2.3.4/32"))
     }
 
     @Test
     fun `Add whitelist de-duplicates IPs sent to Hetzner`() {
-        service.whitelistIpForSession("game/200", userId = 123, ipAddress = "1.2.3.4")
-        service.whitelistIpForSession("game/201", userId = 123, ipAddress = "1.2.3.4")
-        runBlocking { waitUntil { hetznerApi.getCallCount() == 1 } }
+        awaitAll(service.whitelistIpForSession("game/200", userId = 123, ipAddress = "1.2.3.4"))
+
+        awaitAll(service.whitelistIpForSession("game/201", userId = 123, ipAddress = "1.2.3.4"))
 
         val allowedIps = hetznerApi.getRulesByFirewallId("fwid")!!.allSourceIps()
         assertThat(allowedIps).isEqualTo(setOf("1.2.3.4/32"))
@@ -75,17 +77,16 @@ class HetznerFirewallServiceTest {
 
     @Test
     fun `Add whitelist re-uses existing active session`() {
-        service.whitelistIpForSession("game/200", userId = 123, ipAddress = "1.2.3.4")
-        service.whitelistIpForSession("game/200", userId = 123, ipAddress = "1.2.3.4")
-        runBlocking { waitUntil { hetznerApi.getCallCount() == 1 } }
+        awaitAll(service.whitelistIpForSession("game/200", userId = 123, ipAddress = "1.2.3.4"))
+
+        awaitAll(service.whitelistIpForSession("game/200", userId = 123, ipAddress = "1.2.3.4"))
 
         assertThat(firewallWhitelistRepository.getAllActive()).hasSize(1)
     }
 
     @Test
     fun `Add whitelist IPv6`() {
-        service.whitelistIpForSession("game/200", userId = 123, ipAddress = "abcd::")
-        runBlocking { waitUntil { hetznerApi.getCallCount() == 1 } }
+        awaitAll(service.whitelistIpForSession("game/200", userId = 123, ipAddress = "abcd::"))
 
         val allowedIps = hetznerApi.getRulesByFirewallId("fwid")!!.allSourceIps()
         assertThat(allowedIps).isEqualTo(setOf("abcd::/128"))
@@ -93,11 +94,9 @@ class HetznerFirewallServiceTest {
 
     @Test
     fun `Remove user whitelist`() {
-        service.whitelistIpForSession("game/200", userId = 123, ipAddress = "1.2.3.4")
-        runBlocking { waitUntil { hetznerApi.getCallCount() == 1 } }
+        awaitAll(service.whitelistIpForSession("game/200", userId = 123, ipAddress = "1.2.3.4"))
 
-        service.removeWhitelistForSessionUser(123, "game/200")
-        runBlocking { waitUntil { hetznerApi.getCallCount() == 2 } }
+        awaitAll(service.removeWhitelistForSessionUser(123, "game/200"))
 
         val allowedIps = hetznerApi.getRulesByFirewallId("fwid")!!.allSourceIps()
         assertThat(allowedIps).isEmpty()
@@ -105,12 +104,14 @@ class HetznerFirewallServiceTest {
 
     @Test
     fun `Remove session whitelist`() {
-        service.whitelistIpForSession("game/200", userId = 123, ipAddress = "1.2.3.4")
-        service.whitelistIpForSession("game/200", userId = 234, ipAddress = "2.3.4.5")
-        runBlocking { waitUntil { hetznerApi.getCallCount() == 1 } }
+        awaitAll(
+            service.whitelistIpForSession("game/200", userId = 123, ipAddress = "1.2.3.4"),
+            service.whitelistIpForSession("game/200", userId = 234, ipAddress = "2.3.4.5"),
+        )
 
-        service.removeWhitelistsForSession("game/200")
-        runBlocking { waitUntil { hetznerApi.getCallCount() == 2 } }
+        awaitAll(
+            service.removeWhitelistsForSession("game/200"),
+        )
 
         val allowedIps = hetznerApi.getRulesByFirewallId("fwid")!!.allSourceIps()
         assertThat(allowedIps).isEmpty()
@@ -119,8 +120,8 @@ class HetznerFirewallServiceTest {
     @Test
     fun `Whitelist syncing is rate-limited`() {
         // Make multiple requests in quick succession
-        service.whitelistIpForSession("game/201", userId = 123, "1.2.3.4")
-        service.whitelistIpForSession("game/201", userId = 234, "2.3.4.5")
+        val uni1 = service.whitelistIpForSession("game/201", userId = 123, "1.2.3.4")
+        val uni2 = service.whitelistIpForSession("game/201", userId = 234, "2.3.4.5")
 
         runBlocking {
             waitUntil {
@@ -130,35 +131,32 @@ class HetznerFirewallServiceTest {
 
         var allowedIps = hetznerApi.getRulesByFirewallId("fwid")!!.allSourceIps()
         assertThat(allowedIps).hasSize(2)
-        assertThat(hetznerApi.getCallCount()).isEqualTo(1)
+        awaitAll(uni1, uni2)
 
-        service.whitelistIpForSession("game/201", userId = 345, "3.4.5.6")
-        service.whitelistIpForSession("game/202", userId = 456, "4.5.6.7")
-        service.removeWhitelistForSessionUser(234, "game/201")
+        val uni3 = service.whitelistIpForSession("game/201", userId = 345, "3.4.5.6")
+        val uni4 = service.whitelistIpForSession("game/202", userId = 456, "4.5.6.7")
+        val uniRemove = service.removeWhitelistForSessionUser(234, "game/201")
 
+        val currentCount = hetznerApi.getCallCount()
         runBlocking {
             waitUntil {
-                hetznerApi.getCallCount() == 2
+                hetznerApi.getCallCount() == currentCount + 1
             }
         }
 
         allowedIps = hetznerApi.getRulesByFirewallId("fwid")!!.allSourceIps()
         assertThat(allowedIps).hasSize(3)
-        assertThat(hetznerApi.getCallCount()).isEqualTo(2)
+        awaitAll(uni3, uni4, uniRemove)
     }
 
     @Test
     fun `IPs split across multiple rules`() {
-        service.whitelistIpForSession("game/201", userId = 123, "1.2.3.4")
-        service.whitelistIpForSession("game/201", userId = 234, "2.3.4.5")
-        service.whitelistIpForSession("game/202", userId = 345, "3.4.5.6")
-        service.whitelistIpForSession("game/202", userId = 456, "4.5.6.7")
-
-        runBlocking {
-            waitUntil {
-                hetznerApi.getCallCount() == 1
-            }
-        }
+        awaitAll(
+            service.whitelistIpForSession("game/201", userId = 123, "1.2.3.4"),
+            service.whitelistIpForSession("game/201", userId = 234, "2.3.4.5"),
+            service.whitelistIpForSession("game/202", userId = 345, "3.4.5.6"),
+            service.whitelistIpForSession("game/202", userId = 456, "4.5.6.7"),
+        )
 
         assertThat(hetznerApi.getRulesByFirewallId("fwid")).isEqualTo(
             listOf(
@@ -172,3 +170,9 @@ class HetznerFirewallServiceTest {
 }
 
 fun List<FirewallRule>.allSourceIps(): Set<String> = flatMap { it.sourceIps }.toSet()
+
+private fun <T> awaitAll(vararg unis: Uni<T>) {
+    for (uni in unis) {
+        uni.await().atMost(Duration.ofSeconds(10))
+    }
+}
